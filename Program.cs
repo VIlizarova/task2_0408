0e3894041dfbcc6fd89896b823dc7551c462ce38
Valeria_Ilizarova <v.ilizarova@gmail.com>
2022-08-08 11:48:27 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine ("Введите число х ");
+
+int numberX = Convert.ToInt32(Console.ReadLine ());
+
+Console.WriteLine ("Введите число y ");
+
+int numberY = Convert.ToInt32(Console.ReadLine ());
+
+if (numberX == numberY)
+{
+    Console.WriteLine ("numberX = numberY");
+}
+else if (numberX > numberY)
+{
+    Console.WriteLine ("numberX = max, numberY = min");
+}
+else
+{
+Console.WriteLine ("numberY = max, numberX = min");
+}
+
